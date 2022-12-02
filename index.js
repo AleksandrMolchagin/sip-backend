2b6d367ccee093a923ee483495f6589d7043ace6
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-12-02 17:39:06 -0500

    Add extraction from the website

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const LOCAL_HOST_PORT = 4000;
 var express = require("express");
 const axios = require("axios");
+extractor = require("unfluff");
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 var app = express();
 
 app.get("/", async function (req, res) {
-  const data = await getOpenAiResponse("Hello there");
+  const data = await extractTextFromTheUrl(
+    "https://www.healthline.com/health/becoming-vegetarian"
+  );
+
   res.send(data);
 });
 
@@ -48,7 +52,7 @@ async function getGoogleResponse(text) {
 /**
  *  This function will get the response from openai based on the query
  *
- * @param {*} text 
+ * @param {*} text
  * @returns response
  */
 async function getOpenAiResponse(text) {
@@ -70,4 +74,31 @@ async function getOpenAiResponse(text) {
   const data = response.data.choices[0].text;
 
   return data;
+}
+
+/**
+ * 
+ * A function that extracts text from a url 
+ *
+ * @param {*} url 
+ * @returns 
+ */
+async function extractTextFromTheUrl(url) {
+  let data = "";
+  await axios
+    .get(url, {
+      headers: {
+        "accept-encoding": "*",
+      },
+    })
+    .then((response) => {
+      data = extractor(response.data);
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(data);
+  return data.text;
 }
